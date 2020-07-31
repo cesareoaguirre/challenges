0d0bb4a73d49b982a3e954dce17aa468cfbf1ccb
cesareoaguirre <cesareoaguirre@outlook.com>
2020-07-30 23:03:39 -0500

    Prueba realizada el 30 de julio del 2020.
    Preliminar en PERL.
    
    Requiere instalar JSON vía CPAN

---
+++ resuelve/prueba_backend/CalcularSueldo.pl
@@ -0,0 +1,48 @@
+# Prueba Backend Resuelve versión PERL
+# # @CesareoAguirre
+# 30 de julio del 2020
+#
+# PROGRAMACIÓN FUNCIONAL
+#
+# Importar JSON
+# Convertir el INPUT en JSON
+# Procesar cada entrada del JSON como un objeto
+#
+use JSON;
+use Data::Dumper;
+
+# Nuestra fuente de datos es pasada por STDIN (el .pl puede ser llamado por otro módulo).
+# .
+$json = JSON->new->allow_nonref;
+$json_txt = do { local $/; <> };
+$json_obj = decode_json( $json_txt );
+#
+# Los niveles de goles al mes. Aclarar que este es el mejor escenario y pasar un nivel inexistente provocará falla en el script en tiempo de ejecución.
+$NIVELES ={
+	A=>5,
+	B=>10,
+	C=>15,
+	Cuauh=>20,
+};
+# Iteramos en cada jugador y asignamos su sueldo total y sus goles mínimos.
+for my $jugador ( @$json_obj ) {
+	my $nivel= %$jugador{'nivel'};
+	my $goles_minimos= %$NIVELES{ $nivel };
+	my $goles = %$jugador{'goles'}; 
+	my $alcance_bono= $goles / $goles_minimos;
+ 	my $sueldo= %$jugador{'sueldo'};
+	my $bono=  %$jugador{'bono'};
+	my $bono_total= $bono * $alcance_bono; 
+	my $sueldo_total= $sueldo + $bono;
+	$jugador->{'goles_minimos'}=$goles_minimos;
+	$jugador->{'sueldo_total'}=$sueldo_total;
+	#print "Jugador " . %$jugador{'nombre'} . "\n";
+	#print "nivel: $nivel, goles: $goles / $goles_minimos = $alcance_bono \n";
+	#print "bono: $bono\n";
+	#print "sueldo total $sueldo + $bono_total";
+	#print "-----------------\n";
+
+}
+# Lo enviamos a STDOUT
+$salida= $json->pretty->encode( $json_obj );
+print STDOUT $salida;
