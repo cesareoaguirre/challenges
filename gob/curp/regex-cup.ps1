f5a9f98e1df6baf3c20028175d10a0ce592b6f7e
Cesar Aguirre Arvizu <cesar.aguirre.arvizu@hotmail.com>
2021-02-03 17:28:08 -0600

    Oops, para regex-cup.ps1 mee quivoqué en el curp de ejemplo.

--- gob/curp/regex-cup.ps1
+++ gob/curp/regex-cup.ps1
@@ -1,6 +1,6 @@
 # @cesareoaguirre
 # el objetivo de este script es generar un RegEx para verificación de CURP en México
 # Este caso es para una verificación en PoweShell
-$curp="RXVI490617HSPDSS05"
+$curp="ROVI490617HSPDSS05"
 $curp -match '[a-z][aeiou][a-z][a-z]\d\d\d\d\d\d[hm][a-z][a-z][b-df-hj-np-tv-z][b-df-hj-np-tv-z][b-df-hj-np-tv-z]\w\d'
 $curp -match '[a-z][aeiou][a-z]{2}\d{6}[hm][a-z]{2}[b-df-hj-np-tv-z]{3}\w\d'
